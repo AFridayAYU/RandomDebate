40194cc2075f970051b103ba3681ec44c7eacef5
parkjunyeoung <wnsdud0225@naver.com>
2024-07-15 23:01:21 +0900

    페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Secondpage from './page/Secondpage';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const navigate = useNavigate();
 
   return (
     <>
       <h1>Random Debate</h1>
       <div style = {{ display: 'flex', flexDirection : 'column'}}>
-        <button>
+        <button onClick={() => navigate('/Secondpage')}>
           토론방 만들기
         </button>
         <button>
@@ -21,4 +25,13 @@ function App() {
   )
 }
 
-export default App
+export default function AppWrapper() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Secondpage" element={<Secondpage />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/page/Secondpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Secondpage() {
+    return (
+        <div>
+            <h1>토론방 생성</h1>
+            <p>코드번호 ....</p>
+        </div>
+    )
+}
+
+export default Secondpage;
