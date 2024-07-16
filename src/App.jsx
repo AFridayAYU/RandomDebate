87fe327a570b0e34388c27ca51b35522e3bc3e28
rdg1029 <wcle3456@gmail.com>
2024-07-16 15:27:18 +0900

    Main.jsx 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Main from './page/Main';
 import Secondpage from './page/Secondpage';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 
-
-function App() {
-  const [count, setCount] = useState(0)
-  const navigate = useNavigate();
-
-  return (
-    <>
-      <h1>Random Debate</h1>
-      <div style = {{ display: 'flex', flexDirection : 'column'}}>
-        <button onClick={() => navigate('/Secondpage')}>
-          토론방 만들기
-        </button>
-        <button>
-          토론방 참여
-        </button>
-      </div>
-    </>
-  )
-}
-
-export default function AppWrapper() {
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Main />} />
         <Route path="/Secondpage" element={<Secondpage />} />
       </Routes>
     </Router>
---
+++ src/page/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Main() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1>Random Debate</h1>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <button onClick={() => navigate('/Secondpage')}>
+          토론방 만들기
+        </button>
+        <button>
+          토론방 참여
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Main;
