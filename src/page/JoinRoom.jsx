adc496e6c2a46ea75e55a4ce11571953e66eed0c
parkjunyeoung <wnsdud0225@naver.com>
2024-08-04 23:01:22 +0900

    게임화면 연결 1

--- src/page/JoinRoom.jsx
+++ src/page/JoinRoom.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import BackButton from '../components/BackButton';
 import { createClient } from '@supabase/supabase-js';
 import { useNavigate } from 'react-router-dom';
+import ShowTeam from "../components/ShowTeam";
 
 function Thirdpage() {
+    const [isStarted, setIsStarted] = useState(false);
     const codeInputRef = useRef();
     const navigate = useNavigate();
 
@@ -32,15 +34,26 @@ function Thirdpage() {
                         payload: { message: 'CONNECT' },
                     });
                 });
+                setIsStarted(true);
             }
         })
     }
     return(
         <>
+        {isStarted ? (
+            <div>
+                <h2>토론 주제</h2>
+                <ShowTeam isPros={Math.floor(Math.random() * 2)} />
+            </div>
+
+        ):(
+            <div>
         <h2>코드입력</h2>
         <input type="text" maxlength="6" ref={codeInputRef}/>
         <button onClick={onClick}>시작</button>
         <BackButton/>
+        </div>
+    )}
 
         </>
     )
