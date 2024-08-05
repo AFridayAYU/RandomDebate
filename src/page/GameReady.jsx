2f8c59c37505af644fed50e669b5f516f32d6290
parkjunyeoung <wnsdud0225@naver.com>
2024-08-05 16:59:23 +0900

    타이머 기능 설정

--- src/page/GameReady.jsx
+++ src/page/GameReady.jsx
@@ -1,15 +1,26 @@
-import { useContext } from "react";
+import { useContext , useEffect, useState} from "react";
 import ShowTeam from "../components/ShowTeam";
 import { AppContext } from "../App";
 
 export default function GameReady() {
-    const {topic, team} = useContext(AppContext);
+    const {setPage, topic, team} = useContext(AppContext);
+    const [timer, setTimer] = useState(5);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setPage("test");
+        }, 5000)
+
+    }, []);
 
     return (
         <>
         <h2>토론 주제</h2>
         <h2>{topic}</h2>
         <ShowTeam team={team} />
+        <h2> 5초후 게임이 실행됩니다.</h2>
+        
+        
         </>
     )
 }
