dd13870ebb6ef7ea9273249b71aeff117ab2676c
rdg1029 <wcle3456@gmail.com>
2024-08-10 14:55:27 +0900

    테스트 페이지 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { createContext, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
 
 import Main from './page/Main';
 import CreateRoom from './page/CreateRoom';
@@ -16,10 +15,9 @@ export default function App() {
   const [page, setPage] = useState("");
   const [team, setTeam] = useState("");
   const [topic, setTopic] = useState("");
-  const supabase = createClient("https://dyjdsfiutbdavklmudjw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5amRzZml1dGJkYXZrbG11ZGp3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTYyOTE3OCwiZXhwIjoyMDM3MjA1MTc4fQ.qD5MV7L_8IFvbDa27t2Trk0YJdtYPRoflywcuwGi8XI");
 
   return (
-    <AppContext.Provider value={{page, setPage, team, setTeam, topic, setTopic, supabase}}>
+    <AppContext.Provider value={{page, setPage, team, setTeam, topic, setTopic}}>
       {
         page === "create" ? 
         <CreateRoom />
--- src/page/test.jsx
+++ src/page/test.jsx
@@ -1,5 +1,13 @@
+import { useContext } from "react";
+import Timer from "../components/Timer";
+import { AppContext } from "../App";
+
 export default function Test() {
+    const {setPage} = useContext(AppContext)
     return (
-        <h1>TEST</h1>
+        <>
+            <h1>TEST</h1>
+            <Timer ms={30*1000} onTimerEnd={() => setPage("main")} />
+        </>
     )
 }
