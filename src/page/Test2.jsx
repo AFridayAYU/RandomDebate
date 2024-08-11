3b0ac8d20a57a8b77fec43fdf097fdf5769dab21
parkjunyeoung <wnsdud0225@naver.com>
2024-08-11 22:12:56 +0900

    입론 약식 구성

---
+++ src/page/Test2.jsx
@@ -0,0 +1,8 @@
+export default function Test2() {
+    return(
+        <>
+        <h2>test2</h2>
+        </>
+    )
+
+}
--- src/page/test.jsx
+++ src/page/test.jsx
@@ -1,5 +1,24 @@
+import { useContext , useEffect, useState} from "react";
+import { AppContext } from "../App";
+
+
 export default function Test() {
+    const [inputText, setInputText] = useState('');
+    const handleInputChange = (event) => {
+        setInputText(event.target.value);
+    }
+    const {setPage, topic, team} = useContext(AppContext);
+
     return (
-        <h1>TEST</h1>
+        <>
+        <h2>{topic}</h2>
+        <h2>양측 입론</h2>
+        <textarea placeholder="입론을 작성하세요!"
+        value={inputText}
+        onChange={handleInputChange}
+        rows="15"
+        cols="70"></textarea>
+        <button onClick={() => setPage("test2")}>완료</button>
+        </>
     )
 }
