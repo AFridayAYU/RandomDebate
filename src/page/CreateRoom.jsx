1df9c4a01e8d4a3ac0ba29aae28ae8979d963ff4
rdg1029 <wcle3456@gmail.com>
2024-10-08 15:58:17 +0900

    방 생성 페이지 디자인 수정

--- src/page/CreateRoom.jsx
+++ src/page/CreateRoom.jsx
@@ -5,13 +5,13 @@ import supabase from '../supabase';
 
 function CreateRoom() {
     const codeRef = useRef();
-    const {topicAll, setPage, setTeam, setTopic, setChannel, setCode} = useContext(AppContext);
+    const { topicAll, setPage, setTeam, setTopic, setChannel, setCode } = useContext(AppContext);
 
     useEffect(() => {
         console.log('생성');
-        codeRef.current = Math.random().toString(36).substring(2,8)
+        codeRef.current = Math.random().toString(36).substring(2, 8)
         setCode(codeRef.current);
-        supabase.from('room_list').insert({id: codeRef.current, start: false}).then((err) => {
+        supabase.from('room_list').insert({ id: codeRef.current, start: false }).then((err) => {
             const channel = supabase.channel(codeRef.current);
             console.log(supabase.getChannels());
             setChannel(channel);
@@ -30,10 +30,12 @@ function CreateRoom() {
                         channel.send({
                             type: 'broadcast',
                             event: 'topic',
-                            payload: { message: {
-                                team: team === 0 ? 1 : 0,
-                                topic : selectedTopic
-                            } },
+                            payload: {
+                                message: {
+                                    team: team === 0 ? 1 : 0,
+                                    topic: selectedTopic
+                                }
+                            },
                         }).then((result) => {
                             if (result === "ok") setPage("ready");
                         });
@@ -47,14 +49,20 @@ function CreateRoom() {
         }
     }, []);
     return (
-        <>
-        <h2>초대코드</h2>
-        {codeRef.current}
-        <BackButton onClick={() => supabase.from('room_list').delete().eq('id', codeRef.current).then(() => {
-            supabase.removeAllChannels();
-            setPage("main");
-        })} />
-        </>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+        }}>
+            <h2>초대코드</h2>
+            <p style={{
+                fontWeight: "bold",
+                fontSize: "2rem",
+            }}>{codeRef.current}</p>
+            <button onClick={() => supabase.from('room_list').delete().eq('id', codeRef.current).then(() => {
+                supabase.removeAllChannels();
+                setPage("main");
+            })}>뒤로가기</button>
+        </div>
     )
 }
 
