67eec06dc4610c68832df2c2d0d2c9e857da70db
rdg1029 <wcle3456@gmail.com>
2024-11-12 14:23:03 +0900

    대기 화면 말풍선 적용

--- src/page/GameReady.jsx
+++ src/page/GameReady.jsx
@@ -19,7 +19,19 @@ export default function GameReady({ progList }) {
             <h2>{topic}</h2>
             <ShowTeam team={team} />
             <ul>
-                {chat.map((text, idx) => <li key={idx}>{text}</li>)}
+            {chat.map((text, idx) => <div style={{
+                backgroundColor: `${text.split('₩', 1)[0].includes('찬성') ? "#d6efe7" : "#f5cfd3"}`,
+                padding: "10px",
+                margin: "10px",
+                borderRadius: "16px",
+                listStyleType: "unset",
+            }} key={idx}>
+                <div style={{textAlign: `${text.split('₩', 1)[0].includes('찬성') ? "left" : "right"}`,}}>
+                    <b>{text.split('₩', 1)[0]}</b>
+                </div>
+                <p style={{textAlign: 'left'}}>{text.split('₩', 2)[1]}</p>
+            </div>
+            )}
             </ul>
             <h2>{`5초후 ${progList[progress]}이 진행됩니다.`}</h2>
         </>
